867e82746c852102a4e055cb16fdf0a2d04256da
m-shehzad455 <shehzuali5@gmail.com>
2024-01-29 21:55:09 +0500

    update accordian project react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Accordian from "./components/accordian";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Accordian />ordian
+     
+    </div>
+  );
+}
+
+export default App;
